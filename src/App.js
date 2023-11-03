25940e8ea3cffd90f43e70f47e958557f2ab7f3b
gupark <ghp720@naver.com>
2023-11-03 23:59:35 +0900

    feat: 지불 금액 입력 받기 및 유효성 검증

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import Lotto from "./Lotto.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  validateMoney (money){
+    if (money === "") throw new Error("[ERROR] 입력값이 없습니다.");
+    if (money.includes(' ')) throw new Error("[ERROR] 공백을 제외하고 입력해주세요.");
+    if (isNaN(money)) throw new Error("[ERROR] 숫자만 입력해주세요.");
+    if (parseInt(money) % 1000 !== 0) throw new Error ("[ERROR] 천 원 단위로 입력해주세요.");
+    return (parseInt(money) / 1000);
+  }
+
+  async inputMoney() {
+    try{
+      const money = await MissionUtils.Console.readLineAsync('구입 금액을 입력하세요.\n');
+      const affordable = this.validateMoney(money);
+      return affordable;
+    } catch(error){
+      MissionUtils.Console.print(error.message);
+    }
+  }
+  
+  async play() {
+    //const tickets = await this.inputMoney();
+    //MissionUtils.Console.print(tickets);
+  }
 }
 
 export default App;
